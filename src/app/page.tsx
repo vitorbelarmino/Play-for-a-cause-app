752684bed024d9d89af42c5285d215e00ab26e30
vitorbelarmino <vitor.belarmino@hotmail.com>
2023-12-02 22:02:59 -0300

    feat: create login page

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { FaLock, FaUser } from "react-icons/fa";
+
+export default function Login() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col justify-center w-80 h-64 items-center gap-3 p-7 bg-zinc-700 ">
+        <h1>login</h1>
+        <div className="flex items-center gap-1 w-full bf">
+          <FaUser />
+          <input className="w-full" type="text" />
+        </div>
+        <div className="flex items-center gap-1 w-full">
+          <FaLock />
+          <input className="w-full" type="text" />
+        </div>
+        <button className="bg-slate-500 w-full">Login</button>
+        <div className="flex gap-1">
+          <p>Não possui uma conta?</p>
+          <a className="text-blue-700" href="#">
+            crie agora
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
